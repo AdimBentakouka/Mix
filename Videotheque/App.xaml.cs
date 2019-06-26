f80e4c70285301c9a08f966c52ad2cbfb7c1b3bd
Adel BENTAKOUKA <ad.bentakouka@gmail.com>
2019-06-26 21:41:16 +0200

    fix filmservice2

--- Videotheque/App.xaml.cs
+++ Videotheque/App.xaml.cs
@@ -5,13 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-<<<<<<< HEAD
 using Videotheque.Model;
 using Videotheque.Service;
-=======
-using Videotheque.DataAccess;
-using Videotheque.Model;
->>>>>>> 8f90d8f32193b43180c44ad5ba8af7c86ee08a59
+
 
 namespace Videotheque
 {
