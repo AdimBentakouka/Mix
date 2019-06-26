d12542f5aed76e8f6a2b2108206e04971d1bbd1c
Adel BENTAKOUKA <ad.bentakouka@gmail.com>
2019-06-26 22:13:02 +0200

    liaison media_genre

--- Videotheque/App.xaml.cs
+++ Videotheque/App.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using Videotheque.Model;
 using Videotheque.Service;
-
+using Videotheque.DataAccess;
 
 namespace Videotheque
 {
@@ -23,7 +23,13 @@ namespace Videotheque
             base.OnStartup(e);
 
             FilmService test = new FilmService();
-            await test.AddFilm("Mon film", 5, "test syno", 10, 2);
+            var context = await DataAccess.BooksDbContext.GetCurrent();
+
+            Genre genre = new Genre { Nom = "Horreur" };
+            context.Genres.Add(genre);
+
+            await test.AddFilm("Mon film", 5, "test syno", 10, 3);
+            await context.SaveChangesAsync();
         }
 
     }
