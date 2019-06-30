f18bfcc9704a06855a6b78a1ebe6dbe7522a34db
Adel BENTAKOUKA <ad.bentakouka@gmail.com>
2019-06-30 15:36:46 +0200

    edit des tests

--- Videotheque/App.xaml.cs
+++ Videotheque/App.xaml.cs
@@ -37,18 +37,18 @@ namespace Videotheque
             await genreservice.AddGenre("Fantasy");
             await genreservice.AddGenre("Horreur");
 
-            await filmservice.AddFilm("Film 1", 5, "action", 10, 1);
-            await filmservice.AddFilm("Film 2", 1, "aventure", 10, 2);
-            await filmservice.AddFilm("Film 3", 2, "fantasy", 10, 3);
-            await filmservice.AddFilm("Film 4", 5, "action", 10, 1);
-            await filmservice.AddFilm("Film 5", 4, "aventure", 10, 2);
+            await filmservice.AddFilm("Film 1", "action", 10, 1);
+            await filmservice.AddFilm("Film 2", "aventure", 10, 2);
+            await filmservice.AddFilm("Film 3", "fantasy", 10, 3);
+            await filmservice.AddFilm("Film 4", "action", 10, 1);
+            await filmservice.AddFilm("Film 5", "aventure", 10, 2);
             List<Film> filmAction = await filmservice.GetAllFilmByGenre(1);
             List<Film> filmAventure = await filmservice.GetAllFilmByGenre(2);
             List<Film> filmFantasy = await filmservice.GetAllFilmByGenre(3);
             List<Film> filmHorreur = await filmservice.GetAllFilmByGenre(4);
 
             await filmservice.DeleteFilm(1);
-            await filmservice.EditFilm(2,"Film 2 edit", 0, "action", 10, 1);
+            await filmservice.EditFilm(2,"Film 2 edit", "action", 10, 1);
             foreach (Film f in filmFantasy)
             {
                 Console.WriteLine(f.Note + " " + f.Commentaire);
